2ae8c148c14cfce671d79006c8ba8168f3bab87e
Sirarpi Manukyan <manukyansirarpi@gmail.com>
2025-11-25 16:06:43 +0400

    image upload , vervell blob

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
-import type { NextConfig } from "next";
-import { dirname } from "node:path";
 import assert from "node:assert";
+import { dirname } from "node:path";
+import type { NextConfig } from "next";
 
 assert(process.env.BLOB_BASE_URL, "you must have defined BLOB_BASE_URL");
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL(`${process.env.BLOB_BASE_URL}`)],
+    remotePatterns: [new URL(`${process.env.BLOB_BASE_URL}/**`)],
   },
   turbopack: {
     root: dirname(__filename),
